e6170f3ea878117a129ff8603230d273d6ee76cb
eternaltyro <eternaltyro@gmail.com>
2019-01-27 21:32:52 +0530

    Move data to own directory

--- geo-data/constants.py
+++ geo-data/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-DATAFILE = 'libraries_new.geojson'
-TESTFILE = 'libraries_test.geojson'
+DATAFILE = 'data/libraries_new.geojson'
+TESTFILE = 'data/libraries_test.geojson'
 GEODATAFILE = TESTFILE  # Switch between test data and actual data
 COORD_SYSTEM = 'decimal'  # degree or decimal
