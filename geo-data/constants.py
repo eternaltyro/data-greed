53550a9c125003ad5e55cd3d126e903320a87485
eternaltyro <eternaltyro@gmail.com>
2019-07-17 10:57:26 +0530

    Bump all dependency versions

--- geo-data/constants.py
+++ geo-data/constants.py
@@ -2,5 +2,5 @@
 
 DATAFILE = 'data/libraries_new.geojson'
 TESTFILE = 'data/libraries_test.geojson'
-GEODATAFILE = TESTFILE  # Switch between test data and actual data
+GEODATAFILE = DATAFILE  # Switch between test data and actual data
 COORD_SYSTEM = 'decimal'  # degree or decimal
